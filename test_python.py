c8579e4652e2b405055ec1a17db8bb1431e56582
root <root@localhost.localdomain>
2019-09-16 03:38:23 -0400

    Installation script and pytest file

---
+++ test_python.py
@@ -0,0 +1,47 @@
+import subprocess
+import httplib
+import json
+import pytest
+
+def install_foglamp():
+
+	subprocess.call(['./install.sh'])
+
+
+def test_foglamp():
+
+	print ("Validating if FogLAMP is up ....")
+
+	#init_val = {'uptime': 17762, 'safeMode': False, 'dataSent': 0, 'hostName': 'localhost.localdomain', 'dataRead': 0, 'serviceName': 'FogLAMP', 'health': 'green', 'ipAddresses': ['192.168.1.140'], 'dataPurged': 0, 'authenticationOptional': True }
+
+	key = ["safeMode","dataSent","hostName","dataRead","serviceName","health","dataPurged","authenticationOptional"]
+	value = [False,0,'localhost.localdomain',0,'FogLAMP','green',0,True]
+
+	con=httplib.HTTPConnection("192.168.1.140:8081")
+	con.request("GET", "/foglamp/ping")
+	resp=con.getresponse()
+	strdata=resp.read().decode()
+	data=json.loads(strdata)
+	
+	for i in range(len(key)):	   
+	   assert data[key[i]] == value[i]
+
+	# assert data['uptime'] >= 1
+	# assert data['safeMode'] == False
+	# assert data['dataSent'] == 0
+	# assert data['hostName'] == 'localhost.localdomain'
+	# assert data['dataRead'] == 0
+	# assert data['serviceName'] == 'FogLAMP'
+	# assert data['health'] == 'green'
+	# assert data['dataPurged'] == 0
+	# assert data['authenticationOptional'] == True
+	
+def main():
+	
+	install_foglamp()
+	test_foglamp()
+	print ("FogLAMP installation and validation complete")
+
+	
+if __name__ == "__main__":
+	main()
