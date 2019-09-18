0c4d03f56cd866641b5a3610d0ea80a13072537b
yashtatkondawar@gmail.com <root@localhost.localdomain>
2019-09-18 05:34:42 -0400

    Made the changes to match with the standard

--- test_python.py
+++ test_python.py
@@ -2,29 +2,33 @@ import subprocess
 import httplib
 import json
 import pytest
+from collections import Counter
 
-def setup_module(module):
-	print ("*******Setting up***************\n")
+class TestClass:
+    def setup_method(self,method):
+        print ("*******Setting up***************\n")
 	subprocess.call(['./install.sh'])
 
 
-def test_foglamp():
-	print ("Validating if FogLAMP is up ....")
+    def test_foglamp(self):
+        print ("Validating if FogLAMP is up ....")
 
-	#init_val = {'uptime': 17762, 'safeMode': False, 'dataSent': 0, 'hostName': 'localhost.localdomain', 'dataRead': 0, 'serviceName': 'FogLAMP', 'health': 'green', 'ipAddresses': ['192.168.1.140'], 'dataPurged': 0, 'authenticationOptional': True }
+        init_val = {'uptime': 17762, 'safeMode': False, 'dataSent': 0, 'hostName': 'localhost.localdomain', 'dataRead': 0, 'serviceName': 'FogLAMP', 'health': 'green', 'ipAddresses': ['192.168.1.140'], 'dataPurged': 0, 'authenticationOptional': True }
 
-	key = ["safeMode","dataSent","hostName","dataRead","serviceName","health","dataPurged","authenticationOptional"]
-	value = [False,0,'localhost.localdomain',0,'FogLAMP','green',0,True]
+        key = ["uptime","safeMode","dataSent","hostName","dataRead","serviceName","health","ipAddresses","dataPurged","authenticationOptional"]
+        value = [1,False,0,'localhost.localdomain',0,'FogLAMP','green',['192.168.1.140'],0,True]
 
-	con=httplib.HTTPConnection("192.168.1.140:8081")
-	con.request("GET", "/foglamp/ping")
-	resp=con.getresponse()
-	strdata=resp.read().decode()
-	data=json.loads(strdata)
-	
-	for i in range(len(key)):	   
-	   assert data[key[i]] == value[i]
+        con=httplib.HTTPConnection("192.168.1.140:8081")
+        con.request("GET", "/foglamp/ping")
+        resp=con.getresponse()
+        strdata=resp.read().decode()
+        data=json.loads(strdata)
 
-def teardown_module(module):
-	print ("\n********Tearing down********")
-	subprocess.call(['./uninstall.sh'])	
+        assert Counter(data.keys()) == Counter(key)
+
+        for i in range(len(key)):
+            assert data[key[i]] >= value[i]
+
+    def teardown_method(self,method):
+        print ("\n********Tearing down********")
+        subprocess.call(['./uninstall.sh'])
